272a1485a62253402a81fb0e599658f873bf68c8
Muhammad Zainul Ahda <zainahda@gmail.com>
2021-10-26 13:30:39 +0700

    membuat component footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    <div class="copyright mt-3">Copyright ⓒ 2021 | Muhammad Zainul Ahda</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+}
+</script>
+
+<style>
+
+</style>
