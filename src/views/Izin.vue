eafb322eb340fa30350ad7c9c69ce162d07f3038
Muhammad Zainul Ahda <zainahda@gmail.com>
2021-10-26 13:32:29 +0700

    membuat rooter untuk halaman perizinan ITBEL Kemenag RI

---
+++ src/views/Izin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="izin">
+    <Permission/>
+    <Footer class="mt-5"/>
+  </div>
+</template>
+
+<script>
+import Permission from '@/components/Permission.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'Izin',
+  components: {
+    Permission,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
